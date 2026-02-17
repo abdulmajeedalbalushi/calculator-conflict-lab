8e218d5b22c62da44aee1750283d78ca6adcdb4f
Codeline <codeline@example.com>
2026-02-17 11:05:03 +0400

    Add sum and difference functionality

--- Calculator/src/BasicCalculator.java
+++ Calculator/src/BasicCalculator.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class BasicCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Addition and Subtraction
+        System.out.println("Enter first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number:");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
 
         scanner.close();
 
