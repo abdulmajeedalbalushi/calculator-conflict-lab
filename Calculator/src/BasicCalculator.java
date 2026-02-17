65582678fd4594e71b0563e6c5345eb449038fb8
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2026-02-17 11:10:07 +0400

    Added both multiplication and division functionalities

--- Calculator/src/BasicCalculator.java
+++ Calculator/src/BasicCalculator.java
@@ -4,9 +4,27 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class BasicCalculator {
     public static void main(String[] args) {
+        // Declare
         Scanner scanner = new Scanner(System.in);
+        double divideResult = 0;
+        double productResult = 0;
 
+
+        // Input
+        System.out.print("Please Enter two numbers separated by space: ");
+        double firstNumber = scanner.nextInt();
+        double secondNumber = scanner.nextInt();
+
+        // Process
+        divideResult = firstNumber / secondNumber;
+        productResult = firstNumber * secondNumber;
+
+        // Output
+        System.out.printf("Division of %f and %f is %.2f%n", firstNumber, secondNumber, divideResult);
+        System.out.printf("Product of %f and %f is %.2f%n", firstNumber, secondNumber, productResult);
+
+        // Closing the pointer to the STDIN buffer.
         scanner.close();
-
+        System.out.println("Existing the application!");
     }
 }
